4a78ea646feb7355c34b27ace37fdd8e8939cc3b
bitibi <bicskeit@gmail.com>
2018-01-15 11:12:55 +0000

    Update StringNullCheck.sql

--- StringNullCheck.sql
+++ StringNullCheck.sql
@@ -4,6 +4,8 @@ When concatenating strings, if any of them is null, the whole concatenation will
 
 To avoid that, check any nullable fields with COALESCE
 
+This is just to show how COALESCE works, for concatenation use function CONCAT instead
+
 Example from Itzik Ben-Gan:T-SQL Fundamentals book
 
 */
